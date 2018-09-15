d94bc1e9c8221e8068a99e893c5895dacdebc611
Jesse Aldridge <jessealdridge@gmail.com>
2018-09-14 23:33:33 -0700

    remove old file

--- tests.py
+++
@@ -1,8 +0,0 @@
-import shutil, os, sys, threading, StringIO
-
-from pinata import pinata
-
-assert pinata.is_letter('a') == True
-assert pinata.is_letter('1') == False
-assert pinata.is_letter('#') == False
-
